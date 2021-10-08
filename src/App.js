3f0f9b1cef0a95609c5f4599a35976b873f34f56
Patryk Oltuch <95oltuchpatryk@gmail.com>
2021-10-08 09:17:39 +0200

    created basic structure of App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    posts: [
+      { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
+      { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
+      { id: 3, title: 'Adam Małysz Zgolił wąs',     intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'}
+    ]
+  }
+
+  postsList = this.state.posts.map( post => (
+    <div key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.intro}</p>
+    </div>
+  ))
+
+  render(){
   return (
     <div className="App">
-
+      {this.postsList}
     </div>
   );
+  }
 }
 
 export default App;
